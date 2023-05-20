930291e58ae3d145cf92ad641d503119da524167
Nizar <nizar.elghazal@student-cs.fr>
2023-05-20 21:07:45 +0200

    card fix

--- src/fr/cs/Group13/myVelib/Cards/Card.java
+++ src/fr/cs/Group13/myVelib/Cards/Card.java
@@ -1,4 +1,9 @@
 package fr.cs.Group13.myVelib.Cards;
 
-public class Card {
+import fr.cs.Group13.myVelib.Bicycle.Bicycle;
+import fr.cs.Group13.myVelib.DockingStation.DockingStation;
+
+public interface Card {
+    double computeCharge(Bicycle b, int start, int end);
+    void updateBalance(DockingStation station) ;
 }

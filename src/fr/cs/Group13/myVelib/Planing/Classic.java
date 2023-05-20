25d6b9a06f2ca08aeb1452d9f3697bfd45bbefd8
oussamakharouiche <oussama.kharouiche@student-cs.fr>
2023-05-20 20:48:26 +0200

    planning strategy and factory

--- src/fr/cs/Group13/myVelib/Planing/Classic.java
+++ src/fr/cs/Group13/myVelib/Planing/Classic.java
@@ -2,7 +2,7 @@ package fr.cs.Group13.myVelib.Planing;
 
 public class Classic implements RidesPlaning{
     @Override
-    public String rideplaning() {
+    public String getYourPlan() {
         return null;
     }
 }

c93f4e9ca036d27732dfa6a71d47fa9ee2e01dd6
Nizar <nizar.elghazal@student-cs.fr>
2023-05-20 22:57:48 +0200

    pricing func change

--- src/fr/cs/Group13/myVelib/Bicycle/Bicycle.java
+++ src/fr/cs/Group13/myVelib/Bicycle/Bicycle.java
@@ -1,4 +1,5 @@
 package fr.cs.Group13.myVelib.Bicycle;
 
 public interface Bicycle{
+
 }
--- src/fr/cs/Group13/myVelib/Cards/PricingVisitor.java
+++ src/fr/cs/Group13/myVelib/Cards/PricingVisitor.java
@@ -4,6 +4,6 @@ import fr.cs.Group13.myVelib.Bicycle.ElectricalBicycle;
 import fr.cs.Group13.myVelib.Bicycle.MechanicalBicycle;
 
 public interface PricingVisitor {
-    double visit(ElectricalBicycle bicycle);
-    double visit(MechanicalBicycle bicycle);
+    double[] visit(ElectricalBicycle bicycle);
+    double[] visit(MechanicalBicycle bicycle);
 }

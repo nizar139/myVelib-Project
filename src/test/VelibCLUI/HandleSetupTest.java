219f0bd0fed59dd99c5ec0b41ff68b653ae2361c
Nizar <nizar.elghazal@student-cs.fr>
2023-05-28 20:16:18 +0200

    idk

--- src/test/VelibCLUI/HandleSetupTest.java
+++ src/test/VelibCLUI/HandleSetupTest.java
@@ -1,7 +1,7 @@
 package test.VelibCLUI;
 
 import fr.cs.Group13.myVelib.System.VelibSystem;
-import fr.cs.Group13.myVelib.VelibCLUI.HandleSetup;
+import fr.cs.Group13.myVelib.VelibCLUI.HandleSetupCommand;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -10,21 +10,21 @@ class HandleSetupTest {
     @Test
     void testHandleSetupWithOneArgument() {
         String[] args = { "oussama" };
-        VelibSystem result = HandleSetup.handleSetup(args);
+        VelibSystem result = HandleSetupCommand.handleSetup(args);
         Assertions.assertNotNull(result);
     }
 
     @Test
     void testHandleSetupWithFiveArguments() {
         String[] args = { "velibnetworkName", "10", "10", "4", "75" };
-        VelibSystem result = HandleSetup.handleSetup(args);
+        VelibSystem result = HandleSetupCommand.handleSetup(args);
         Assertions.assertNotNull(result);
     }
 
     @Test
     void testHandleSetupWithInvalidArguments() {
         String[] args = { "invalid", "ede"};
-        VelibSystem result = HandleSetup.handleSetup(args);
+        VelibSystem result = HandleSetupCommand.handleSetup(args);
         Assertions.assertNull(result);
     }
 
